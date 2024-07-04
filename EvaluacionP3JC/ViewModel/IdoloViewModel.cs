d5ad8ae465532c7e1e7dc558884095e0de9c4fcf
Joaquín Chacón Groes-Petersen <jchacongp3@outlook.com>
2024-07-04 08:52:03 -0500

    falta navegar a la venta del personaje

---
+++ EvaluacionP3JC/ViewModel/IdoloViewModel.cs
@@ -0,0 +1,45 @@
+﻿using EvaluacionP3JC.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvaluacionP3JC.ViewModel
+{
+    public class FavoriteCharacterViewModel : INotifyPropertyChanged
+    {
+        private FavoriteCharacter _favoriteCharacter;
+
+        public FavoriteCharacter FavoriteCharacter
+        {
+            get => _favoriteCharacter;
+            set
+            {
+                if (_favoriteCharacter != value)
+                {
+                    _favoriteCharacter = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public FavoriteCharacterViewModel()
+        {
+            FavoriteCharacter = new FavoriteCharacter
+            {
+                ImageUrl = "https://dt7v1i9vyp3mf.cloudfront.net/styles/news_large/s3/imagelibrary/F/Fred_Again_01-ek5spoxRapFfVohY5SG8oGzj9HFiZGYH.jpg",
+                Description = ""
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ EvaluacionP3JC/Views/IdoloView.xaml.cs
@@ -0,0 +1,9 @@
+namespace EvaluacionP3JC.Views;
+
+public partial class IdoloView : ContentPage
+{
+	public IdoloView()
+	{
+		InitializeComponent();
+	}
+}
