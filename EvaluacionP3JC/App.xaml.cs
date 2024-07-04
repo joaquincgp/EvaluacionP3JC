5338045d682bebdf7c83fb615cfc5304c58449c7
Joaquín Chacón Groes-Petersen <jchacongp3@outlook.com>
2024-07-04 07:15:56 -0500

    Add project files.

---
+++ EvaluacionP3JC/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EvaluacionP3JC
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ EvaluacionP3JC/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EvaluacionP3JC
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
